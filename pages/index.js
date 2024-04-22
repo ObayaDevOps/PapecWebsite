689fa9d27c881af534500aa35df7be7aba3b62d2
ObayaDevOps <obayajamesdralega@gmail.com>
2024-04-22 12:44:01 +0200

    updated metadata

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,17 @@ export default function Home() {
         <title>Home | People and Potential Consultancy</title>
         <meta name="description" content="People and Potential Consultancy" />
         <link rel="shortcut icon" href="../../../images/icon/People__Potential-Logo_Full_Color-1.png"></link>
+
+        <meta property="og:title" content="People and Potential Consultancy" />
+        <meta property="og:description" content="Professional HR Training" />
+        <meta property="og:image" content="https://res.cloudinary.com/medoptics-image-cloud/image/upload/v1713782566/People__Potential-Logo_Full_Color-1_wno2bv.png" />
+        <meta property="og:image:secure_url" content="https://res.cloudinary.com/medoptics-image-cloud/image/upload/v1713782566/People__Potential-Logo_Full_Color-1_wno2bv.png" />
+        <meta property="og:url" content="https://papec-website.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+
+        <link rel="icon" href="/yujo_icon.svg" />
+      
       </Head>
 
       <Box bg={'whiteAlpha.200'}>
